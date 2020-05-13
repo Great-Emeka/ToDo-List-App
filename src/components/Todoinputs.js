8b3c612d9b1509b69f18fb9a9f3933d266b8d4e0
Great-Emeka <greatestgreatemeka@gmail.com>
2020-05-13 22:08:01 +0100

    finished todoIput form

--- src/components/Todoinputs.js
+++ src/components/Todoinputs.js
@@ -9,13 +9,12 @@ export default class todoInputs extends Component {
                     <div className="input-group">
                         <div className="input-group prepend">
                             <div class="input-group-text bg-primary text-white">
-                                
-                            </div>   
+                                <i className ="fas fa-book"></i>
+                            </div>
+                            <input type="text" className="form-control" placeholder="Add a todo item"></input>   
                         </div>
-
                     </div>
                 </form>
-                Hello from todoInputs
             </div>
         )
     }
